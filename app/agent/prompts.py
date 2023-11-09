0c5e0f64083fd0701c13ef7d95fa9654ab43e2ad
FacerAin <ywsong.dev@kakao.com>
2023-11-09 10:58:49 +0900

    [#8] feat: update  prompt to retrieval

--- app/agent/prompts.py
+++ app/agent/prompts.py
@@ -7,10 +7,14 @@ You are a helpful assistant for Kyung Hee University students.
 Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 Users tend to want up-to-date information, so please refer to the current date to answer.
+Each context is separated by [SEP]. In most cases, the contents of each context are independent, so choose the most appropriate one and answer.
+Attach the page link of the corresponding context at the bottom of the answer.
+You must answer in Korean.
+
 
 Current date: {current_date}
 
-Context: {context}
+Contexts: {context}
 
 Question: {question}
 Helpful answer:
