178a0ff85346f95aa3eb99a14fa32f5248e4e38f
FacerAin <ywsong.dev@kakao.com>
2023-11-13 14:34:26 +0900

    feat: add endpoint for similarity search

--- app/api/api_v1/endpoints/chat.py
+++ app/api/api_v1/endpoints/chat.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.agent import ChatAgent
+from app.agent.retriever import PineconeRetriever
 from app.schemas.chat import ResponseAnswer, ReuqestQuery
 
 router = APIRouter()
@@ -12,3 +13,9 @@ chat_agent = ChatAgent()
 def make_chat(req: ReuqestQuery):
     answer = chat_agent.run(req.query)
     return {"answer": answer}
+
+
+@router.get("/similarity-search")
+def get_similarity_search(query: str):
+    retreiver = PineconeRetriever(index_name="khugpt")
+    return retreiver.get_relevant_doc_string(query)
