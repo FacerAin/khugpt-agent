57d3eb49e756a4eb1e183f95f5788e47469428a4
FacerAin <ywsong.dev@kakao.com>
2023-11-30 13:52:13 +0900

    chore: update CORS lists

--- app/core/config.py
+++ app/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost", "http://localhost:46601"]
 
     PROJECT_NAME: str = "KHUGPT"
     OPENAI_API_KEY: str
