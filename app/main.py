b6563dbadbb95c9fbfe78a2e797fff1c8cf39b08
FacerAin <ywsong.dev@kakao.com>
2023-12-05 01:57:34 +0900

    chore: format black

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
@@ -15,8 +17,6 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_methods=["*"],
         allow_headers=["*"],
     )
-
-app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
 @app.get("/")
