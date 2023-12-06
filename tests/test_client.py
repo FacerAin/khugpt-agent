5c8b0215b013472f68c165fdd8e029e873e89484
Yong woo Song <ywsong.dev@kakao.com>
2023-12-07 02:58:20 +0900

    feat: add pytest-dotenv (#30)
    * feat: add pytest-dotenv
    
    * chore: update test deps
    
    * chore: update pinecone test mock
    
    * chore: update test pinecone to mock
    
    * chore: delete test_similarity_search for offline
    
    * chore: update test for online
    
    * feat: add test to FakeRetriever
    
    * feat: update test action for env
    
    * feat: update test action for env

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,4 @@
+import pinecone
 import pytest
 from fastapi.testclient import TestClient
 
@@ -5,30 +6,30 @@ from app.agent import ExecutorAgent
 from app.agent.retriever import PineconeRetriever
 from app.main import app
 
+
+class FakeRetriever(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def get_relevant_doc_string(self, *args, **kwargs):
+        return "Hello"
+
+
 client = TestClient(app)
 
 
 def test_completion(monkeypatch):
-    def mockreturn(a, b):
+    def mockreturn(*args, **kwargs):
         return "Hello"
 
     monkeypatch.setattr(ExecutorAgent, "run", mockreturn)
+    monkeypatch.setattr("app.agent.retriever.PineconeRetriever", FakeRetriever)
+
     req_body = {"query": "Hi"}
     response = client.post("/api/v1/chat/completion", json=req_body)
     assert response.status_code == 200
     assert "answer" in response.json()
     assert "Hello" == response.json()["answer"]
-
-
-def test_similarity_search(monkeypatch):
-    def mockreturn(a, b):
-        return "Hello"
-
-    monkeypatch.setattr(PineconeRetriever, "get_relevant_doc_string", mockreturn)
-    response = client.get("/api/v1/chat/similarity-search?query=Hi")
-    assert response.status_code == 200
-    assert isinstance(response.text, str)
-    assert '"Hello"' == response.text
 
 
 @pytest.mark.live
