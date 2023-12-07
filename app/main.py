235ebe777113f1364cfdddd60f02c4681e50562f
FacerAin <ywsong.dev@kakao.com>
2023-12-07 11:41:44 +0900

    feat: update cors policies

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
